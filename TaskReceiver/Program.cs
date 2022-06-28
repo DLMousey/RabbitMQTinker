fb3730ffedb9f819535a9f2e8671258869d6c349
DLMousey <8281699+DLMousey@users.noreply.github.com>
2022-06-29 00:55:55 +0100

    Add more randomness to timings and failure chance

--- TaskReceiver/Program.cs
+++ TaskReceiver/Program.cs
@@ -16,25 +16,30 @@ channel.QueueDeclare(queue: "TaskQueue",
     autoDelete: false,
     arguments: null);
 
+// Attempts to distribute tasks more evenly, can probably tune this on some nodes running a slightly modified
+// version of this consumer. Might allow for a somewhat selective allocation of jobs to nodes
 channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
 consumer.Received += (model, ea) =>
 {
     byte[] body = ea.Body.ToArray();
     string message = Encoding.UTF8.GetString(body);
-    
-    Thread.Sleep(3000);
 
+    // Add a random chance of simulating failure - completely exit the process and stop the container if it's high enough
     Random random = new Random();
     int nextRandom = random.Next(0, 100);
-
     if (nextRandom >= 80)
     {
         Console.WriteLine($"[TaskReceiver] Instance id: {instanceId} simulating task process failure at {DateTime.Now.ToString()}");
         Environment.Exit(1);
     }
+
+    // Add a random sleep to simulate some jobs being tiny and others being massive (e.g. IO-bound workloads)
+    Thread.Sleep(nextRandom * 100);
     
     Console.WriteLine($"[TaskReceiver] Instance id: {instanceId} received '{message}' w/ delivery tag of {ea.DeliveryTag} at {DateTime.Now.ToString()}");
+    
+    // VERY IMPORTANT - MUST manually send an acknowledgement back to RabbitMQ or it'll assume the task failed and re-queue it
     channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
 };
 
